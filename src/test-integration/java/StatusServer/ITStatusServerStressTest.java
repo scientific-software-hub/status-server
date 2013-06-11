412827c5cd89d8edb1a8230665b4df4dc8beea64
Ingvord <Ingvord@users.noreply.github.com>
2013-06-11 17:45:46 +0200

    StatusServer: performance integration test

---
+++ src/test-integration/java/StatusServer/ITStatusServerStressTest.java
@@ -0,0 +1,30 @@
+package StatusServer;
+
+import org.junit.Test;
+
+/**
+ * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
+ * @since 11.06.13
+ */
+public class ITStatusServerStressTest {
+    @Test
+    public void testGetLatestSnapshot(){
+
+    }
+
+    @Test
+    public void testGetSnapshot(){
+
+    }
+
+    @Test
+    public void testGetDataUpdates(){
+
+    }
+
+    @Test
+    public void testGetData(){
+
+    }
+
+}
