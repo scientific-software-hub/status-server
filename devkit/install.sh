09e1f545ecff3e304789c0e65143fe3daee86935
Ingvord <Ingvord@users.noreply.github.com>
2013-07-16 12:34:57 +0200

    StatusServer: update devkit

--- devkit/install.sh
+++ devkit/install.sh
@@ -15,3 +15,7 @@ mvn install:install-file -Dfile=TangORB-javacommon-8.1.3.jar -DgroupId=org.tango
 mvn install:install-file -Dfile=JavaTangoIDL-4.0.1.jar -DgroupId=org.tango -DartifactId=JavaTangoIDL -Dversion=4.0.1 -Dpackaging=jar -DpomFile=JavaTangoIDL-4.0.1.pom -Dsources=JavaTangoIDL-4.0.1-sources.jar
 
 mvn install:install-file -Dfile=JavaAPI-8.1.3.pom -DgroupId=org.tango -DartifactId=JavaAPI -Dversion=8.1.2 -Dpackaging=pom -DpomFile=JavaAPI-8.1.3.pom
+
+mvn install:install-file -Dfile=utils-0.0.1.jar -DgroupId=hzg.wpn -DartifactId=utils -Dversion=0.0.1 -Dpackaging=jar -DpomFile=utils-0.0.1.pom -Dsources=utils-0.0.1-sources.jar -Djavadoc=utils-0.0.1-javadoc.jar
+
+mvn install:install-file -Dfile=TangoAPI-1.0.2.jar -DgroupId=hzg.wpn -DartifactId=TangoAPI -Dversion=1.0.2 -Dpackaging=jar -DpomFile=TangoAPI-1.0.2.pom -Dsources=TangoAPI-1.0.2-sources.jar -Djavadoc=TangoAPI-1.0.2-javadoc.jar
