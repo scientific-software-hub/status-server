78eb28a878130698f6dc2796055fd2096b97f9b0
ingvord <Ingvord@users.noreply.github.com>
2013-09-27 00:47:32 +0400

    StatusServer: fix devkit install.sh

--- devkit/install.sh
+++ devkit/install.sh
@@ -3,18 +3,10 @@
 export TINE_VER=4.3.9
 echo TINE_VER=$TINE_VER
 
-set JTANGO_VER=1.0.2-alpha-all
+export JTANGO_VER=1.0.2-alpha-all
 echo JTANGO_VER=$JTANGO_VER
 
-mvn install:install-file -Dfile=tineJAVA-$TINE_VER.jar -DgroupId=de.desy.tine -DartifactId=tineJAVA -Dversion=4.3.9 -Dpackaging=jar -DgeneratePom=true
-
-mvn install:install-file -Dfile=JTangoServer-$JTANGO_VER.jar -DgroupId=org.tango -DartifactId=JTangoServer -Dversion=1.0.2-alpha-all -Dpackaging=jar -DgeneratePom=true -Dpackaging=jar
-
-install "utils" "0.0.1"
-
-install "TangoApi" "1.0.4"
-
-function install {
+function _install {
 export ARTIFACT=$1
 export VERSION=$2
 export INSTALL_DIR=/tmp/$ARTIFACT
@@ -24,4 +16,12 @@ mvn install:install-file -Dfile=$INSTALL_DIR/$ARTIFACT-$VERSION.jar -DgroupId=hz
 echo Removing $INSTALL_DIR
 rm -r $INSTALL_DIR
 }
+
+mvn install:install-file -Dfile=tineJAVA-$TINE_VER.jar -DgroupId=de.desy.tine -DartifactId=tineJAVA -Dversion=4.3.9 -Dpackaging=jar -DgeneratePom=true
+
+mvn install:install-file -Dfile=JTangoServer-$JTANGO_VER.jar -DgroupId=org.tango -DartifactId=JTangoServer -Dversion=1.0.2-alpha-all -Dpackaging=jar -DgeneratePom=true -Dpackaging=jar
+
+_install "utils" "0.0.1"
+
+_install "TangoAPI" "1.0.4"
 
