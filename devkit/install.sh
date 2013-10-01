34e3b55665da9d558e57760605ef4e9365aac624
Ingvord <Ingvord@users.noreply.github.com>
2013-10-01 20:25:04 +0200

    StatusServer: fix install.sh

--- devkit/install.sh
+++ devkit/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-export TINE_VER=4.3.9
+echo M2_HOME=$M2_HOME
+
+TINE_VER=4.3.9
 echo TINE_VER=$TINE_VER
 
-export JTANGO_VER=1.0.2-alpha-all
+JTANGO_VER=1.0.2-alpha-all
 echo JTANGO_VER=$JTANGO_VER
 
 function _install {
