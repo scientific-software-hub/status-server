af779dcb2f586932f21a12e2f2c8dc84caa747a9
Igor Khokhriakov <Ingvord@users.noreply.github.com>
2015-03-21 19:11:04 +0100

    remove old tine jars; move hzg-utils to bintray

--- devkit/install.bat
+++ devkit/install.bat
@@ -1,21 +1,6 @@
 @echo off
 
-set TINE_VER=4.5.0
+set TINE_VER=4.5.4
 echo TINE_VER=%TINE_VER%
 
 call mvn install:install-file -Dfile=tineJAVA-%TINE_VER%.jar -DgroupId=de.desy.tine -DartifactId=tineJAVA -Dversion=%TINE_VER% -Dpackaging=jar -DgeneratePom=true
-
-call :install "utils" "0.0.1"
-
-GOTO:EOF
-
-:install
-set ARTIFACT=%~1
-set VERSION=%~2
-set INSTALL_DIR=%TMP%\%ARTIFACT%
-echo Extracting %ARTIFACT%-%VERSION%-bundle.zip into %INSTALL_DIR%
-call unzip %ARTIFACT%-%VERSION%-bundle.zip -d %INSTALL_DIR%
-call mvn install:install-file -Dfile=%INSTALL_DIR%\%ARTIFACT%-%VERSION%.jar -DgroupId=hzg.wpn -DartifactId=%ARTIFACT% -Dversion=%VERSION% -Dpackaging=jar -DpomFile=%INSTALL_DIR%\pom.xml -Dsources=%INSTALL_DIR%\%ARTIFACT%-%VERSION%-sources.jar -Djavadoc=%INSTALL_DIR%\%ARTIFACT%-%VERSION%-javadoc.jar
-echo Removing %INSTALL_DIR%
-call rm -r %INSTALL_DIR%
-GOTO:EOF
--- devkit/install.sh
+++ devkit/install.sh
@@ -2,20 +2,7 @@
 
 echo M2_HOME=$M2_HOME
 
-TINE_VER=4.5.0
+TINE_VER=4.5.4
 echo TINE_VER=$TINE_VER
 
-function _install {
-export ARTIFACT=$1
-export VERSION=$2
-export INSTALL_DIR=/tmp/$ARTIFACT
-echo Extracting $ARTIFACT-$VERSION-bundle.zip into $INSTALL_DIR
-unzip $ARTIFACT-$VERSION-bundle.zip -d $INSTALL_DIR
-$M2_HOME/bin/mvn install:install-file -Dfile=$INSTALL_DIR/$ARTIFACT-$VERSION.jar -DgroupId=hzg.wpn -DartifactId=$ARTIFACT -Dversion=$VERSION -Dpackaging=jar -DpomFile=$INSTALL_DIR/pom.xml -Dsources=$INSTALL_DIR/$ARTIFACT-$VERSION-sources.jar -Djavadoc=$INSTALL_DIR/$ARTIFACT-$VERSION-javadoc.jar
-echo Removing $INSTALL_DIR
-rm -r $INSTALL_DIR
-}
-
 $M2_HOME/bin/mvn install:install-file -Dfile=tineJAVA-$TINE_VER.jar -DgroupId=de.desy.tine -DartifactId=tineJAVA -Dversion=$TINE_VER -Dpackaging=jar -DgeneratePom=true
-
-_install "utils" "0.0.1"
