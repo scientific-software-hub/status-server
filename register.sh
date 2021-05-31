9e16a158bf52297270d253d647e267a1cf414ff5
ingvord <mail@ingvord.ru>
2021-05-31 17:40:18 +0200

    upgrade JTango; add Dockerfile

--- register.sh
+++ register.sh
@@ -1,3 +1,3 @@
 CID=`docker ps -aqf "name=tango-cs"`
 echo $CID
-docker exec $CID /usr/bin/tango_admin --add-server StatusServer2/test StatusServer2 test/status_server/test
+docker exec $CID /usr/local/bin/tango_admin --add-server StatusServer2/dev StatusServer2 dev/xenv/status_server
